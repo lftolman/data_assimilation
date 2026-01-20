f3ed3bc87764008f4715f8e9e4ed8fa35120dc0e
Lydia Tolman <123709251+lftolman@users.noreply.github.com>
2026-01-20 10:31:17 -0700

    Clear setup.py configuration
    Removed all setup parameters from setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="da",
-    version="0.1.0",
-    packages=find_packages(),
-    description="Conditionally Gaussian Data Assimilation methods and experiments",
-    author="Lydia Tolman",
-    install_requires=["numpy","scipy","matplotlib"],
-    python_requires=">=3.10",
-)
+setup()
