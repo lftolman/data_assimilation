4279dec6d71beb02fa4e8ac63be3b0be49542a70
Lydia Tolman <123709251+lftolman@users.noreply.github.com>
2026-01-20 10:23:17 -0700

    Update package name and description in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kalman_bucy_filter",
+    name="da",
     version="0.1.0",
     packages=find_packages(),
-    description="Simple Kalman-Bucy filter for conditionally Gaussian systems",
+    description="Conditionally Gaussian Data Assimilation methods and experiments",
     author="Lydia Tolman",
-    install_requires=["numpy"],
-    python_requires=">=3.9",
+    install_requires=["numpy","scipy","matplotlib"],
+    python_requires=">=3.10",
 )
